912eb662f2091a6cd550881120444d12a822e82a
vishnu yadav <vishnuhurda787@gmail.com>
2024-11-19 11:04:25 +0530

    difficulty problem solved

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 let startBtn = document.querySelector(".start-game");
  let mainContainer = document.querySelector(".main-container")
  let score = document.querySelector(".score")
-  let difficulty = document.querySelector("#difficulty").value;
+ 
   let boxContainer = document.querySelector(".box-container");
  let restart = document.querySelector(".restart")
  let gameOver = document.querySelector(".game-over")
 function getGameData() {
   let numberOfBox;
   let boxNumber =  parseInt(document.querySelector(".box-number").value);
+ let difficulty = document.querySelector("#difficulty").value;
+ let numberOfDangerousBoxes;
 
 if (boxNumber>50 ) {
   numberOfBox = 50;
@@ -21,17 +23,6 @@ if (boxNumber>50 ) {
   restart.style.display = "block"
   boxContainer.style.display = "flex"
   
-let numberOfDangerousBoxes;
-
-
-if (difficulty === "easy") {
-  numberOfDangerousBoxes = Math.floor(numberOfBox / 50);
-} else if (difficulty === "normal") {
-  numberOfDangerousBoxes = Math.floor(numberOfBox / 20);
-} else {
-  numberOfDangerousBoxes = Math.floor(numberOfBox / 2);
-}
-
 let boxes = [];
 for (let i = 0; i < numberOfBox; i++) {
   const box = document.createElement("div");
@@ -39,7 +30,15 @@ for (let i = 0; i < numberOfBox; i++) {
   boxContainer.appendChild(box);
   boxes.push(box);
 }
+if (difficulty === "Easy") {
+  numberOfDangerousBoxes = Math.floor(numberOfBox *0.2);
 
+} else if (difficulty === "Normal") {
+  numberOfDangerousBoxes = Math.floor(numberOfBox *0.3);
+
+}else{
+  numberOfDangerousBoxes = Math.floor(numberOfBox *0.5);
+}
 
 for (let i = 0; i < numberOfDangerousBoxes; i++) {
   let randomIndex = Math.floor(Math.random() * boxes.length);
@@ -47,20 +46,22 @@ for (let i = 0; i < numberOfDangerousBoxes; i++) {
   boxes.splice(randomIndex, 1); 
 }
 
-
 let totalscore = 0;
 document.querySelectorAll(".box").forEach((box) => {
   box.addEventListener("click", function () {
     if (box.classList.contains("dangerous")) {
       box.style.backgroundColor = "red"; 
+      box.style.borderColor = "red"
       gameOver.style.display = "flex"
       document.querySelectorAll(".box").forEach((b) => b.style.pointerEvents = "none");
     } else {
       box.style.backgroundColor = "green"; 
+       box.style.borderColor = "green"
       totalscore += 1;
       score.innerHTML =`SCORE:${totalscore}` ;
       box.style.pointerEvents = "none";
     }
+    box.innerHTML = totalscore;
   });
 });
 }
